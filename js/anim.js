cb59ea4832432cd12afe0888b2f7938f12cac097
tangent <tangentdesign@outlook.com>
2015-03-28 17:23:53 -0700

    mobile and js overhaul

--- js/anim.js
+++ js/anim.js
@@ -1,163 +1,189 @@
+
 /////////////////////////////////
 //      Load page              //
 /////////////////////////////////
 
-$(window).load(function(){
+var load_frame_anim = function(){
 
-  $('.wrapper').velocity({
+  wrapper.velocity({
     opacity: '1',
   },{
     duration: 400 
   });
 
-  $('.load-frame').velocity({
+  load_frame.velocity({
     opacity: '0',
   }, { 
       display: "none",
       duration: 100 
      });
 
-})
+} // load_frame
+
 
 
 /////////////////////////////////
 //      Flip landing frame     //
 /////////////////////////////////
 
-var counter = 2;
+var flip_landing_frame = function(){
 
-$('.lf-button, .service-button').click(function(event) {
+  var counter = 2;
 
-    counter ++;
+  lf_service_button.click(function(event) {
 
-    if (counter % 2 !== 0) {
+      counter ++;
 
-      $('.lf-card, .service-card').velocity({
-        scale  : ".8",
-      },{
-        duration : '300',
-        complete : function(){
+      if (counter % 2 !== 0) {
 
-          $('.lf-card, .service-card').velocity({
-            rotateX : "180deg",
-            scale   : "1",
-          },{
-            duration : '650',
-          })
-        },
-      });
+        lf_service_card.velocity({
+          scale  : ".8",
+        },{
+          duration : '300',
+          complete : function(){
+            lf_service_card.velocity({
+              rotateX : "180deg",
+              scale   : "1",
+            },{
+              duration : '650',
+            })
+          },
+        });
 
-    } else {
+      } else {
 
-      $('.lf-card, .service-card').velocity({
-        scale  : ".8",
-      },{
-        duration : '300',
-        complete : function(){
+        lf_service_card.velocity({
+          scale  : ".8",
+        },{
+          duration : '300',
+          complete : function(){
 
-          $('.lf-card, .service-card').velocity({
-            rotateX : "0deg",
-            scale   : "1",
-          },{
-            duration : '650',
-          })
-        },
-      });
+            lf_service_card.velocity({
+              rotateX : "0deg",
+              scale   : "1",
+            },{
+              duration : '650',
+            })
+          },
+        });
 
-    };
+      };
 
-  });
+    });
+
+} // flip_landing_frame
+
 
 
 /////////////////////////////////
 //      Logo intro animation   //
 /////////////////////////////////
 
-$(window).load(function() {
+var logo_intro_anim = function(){
 
 
-  setTimeout(function() {
+    if ($(window).width() >= 1121){  
 
-    if ($(window).width() >= 820){  
+      var logo_moose         = $('#logo-moose'),
+          logo_holder        = $('#logo-holder'),
+          logo_holder_height = logo_holder.height(),
+          logo_moose_height  = logo_moose.height(),
 
-      var logo_moose     = $('#logo-moose'),
-      logo_holder        = $('#logo-holder'),
-      logo_holder_height = logo_holder.height(),
-      logo_moose_height  = logo_moose.height(),
-      // viewport info
-      lf_wrap            = $('.lf-wrap'),
-      lf_wrap_width      = lf_wrap.width(),
-      lf_wrap_height     = lf_wrap.height(),
-      // top
-      logo_holder_top    = (lf_wrap_height - logo_holder_height) / 2,
-      logo_moose_top     = logo_holder_top + 11;
-      // width
+          // viewport info
+          lf_wrap            = $('.lf-wrap'),
+          lf_wrap_width      = lf_wrap.width(),
+          lf_wrap_height     = lf_wrap.height(),
+          // top
+          logo_holder_top    = (lf_wrap_height - logo_holder_height) / 2,
+          logo_moose_top     = logo_holder_top + 11;
+          // width
 
-      // positioning and scaling defaults
-      logo_moose.css({
-        top       : logo_moose_top,
-      });
+          // positioning and scaling defaults
+          logo_moose.css({
+            top       : logo_moose_top,
+          });
 
-      logo_holder.css({
-        top       : logo_holder_top,
-      });
+          logo_holder.css({
+            top       : logo_holder_top,
+          });
 
 
-      logo_moose.velocity({
-        rotateZ : "360deg",
-        top     : (lf_wrap_height - logo_moose_height) / 2,
-        scale   : 3.1,
-      },{
-        duration  : 1500,
-        easing    : [40, 10],
-        delay     :500,
-        complete  : function(){
-            logo_holder.css('display', 'block');
-        } // callback
+          logo_moose.velocity({
+            rotateZ : "360deg",
+            top     : (lf_wrap_height - logo_moose_height) / 2,
+            scale   : 3.1,
+          },{
+            duration  : 1500,
+            easing    : [40, 10],
+            delay     :500,
+            complete  : function(){
+                logo_holder.css('display', 'block');
+            } // callback
 
-      });
+          });
 
-      logo_moose.velocity({
-        rotateZ : "0deg",
-        top     : logo_moose_top,
-        scale   : 1,
-      },{
-        duration  : 1000,
-        easing    : [40, 10],
-      });
+          logo_moose.velocity({
+            rotateZ : "0deg",
+            top     : logo_moose_top,
+            scale   : 1,
+          },{
+            duration  : 1000,
+            easing    : [40, 10],
+          });
 
-    }; // end if
+        }; // end if
 
-  }, 0); //timeout
-  
-}); //doc ready
 
-  
-/////////////////////////////////
-//      Service Accordian      //
-/////////////////////////////////
-
-var arrow = $('.lf-down-arrow, scroll-txt');
-arrow.click(function() {
-    $('html, body').animate({
-        scrollTop: $("#eastlake").offset().top
-    }, 500);
-});
+} // logo_intro_anim
 
 
 /////////////////////////////////
 //      Scroll animation       //
 /////////////////////////////////
 
-var h2 = $('.service-accordian h2');
+var arrow_scroll = function(){
 
-  h2.next().hide();
-
-  h2.click(function(event) {
-    h2.next().velocity("slideUp", { duration: 200 });
-    $(this).next().velocity("slideDown", { duration: 200 });
+  down_arrow.click(function() {
+    $('html, body').animate({
+        scrollTop: $("#eastlake").offset().top
+    }, 500);
   });
 
+} // arrow_scroll
+
+
+
+/////////////////////////////////
+//      Service Accordian      //
+/////////////////////////////////
+
+var service_accordian = function(){
+
+
+  accordian_h2.next().hide();
+
+  accordian_h2.click(function(event) {
+    accordian_h2.next().velocity("slideUp", { duration: 120 });
+    $(this).next().velocity("slideDown", { duration: 120 });
+  });
+
+} // service_accordian
+
+
+
+/////////////////////////////////
+//      Mobile Size LF         //
+/////////////////////////////////
+
+if ($(window).width() <= 1120){  
+
+  var h = $('.service-accordian').height();
+  $('.lf-wrap, .flipdiv').css({
+    height: h-'100',
+  });
+  alert(h)
+
+} // if
 
 
 
---
+++ js/globals.js
@@ -0,0 +1,13 @@
+// all interactive elements on the page
+var wrapper                 = $('.wrapper'),
+	load_frame              = $('.load-frame'),
+	//lf_wrap                 = $('#logo-moose'),
+	//logo_holder             = $('#logo-holder'),
+	lf_card                 = $('.lf-card'),
+	// lf_button               = $('.lf-button'),
+	service_card            = $('.service-card'),
+	// service_button          = $('.service-button'),
+	lf_service_card         = $('.lf-card, .service-card'),
+	lf_service_button       = $('.lf-button, .service-button'),
+	down_arrow              = $('.lf-down-arrow, scroll-txt'),
+	accordian_h2            = $('.service-accordian h2');
